1a6b7519652ea65d91a444d1fe9ad206b185a748
Ulises Juarez <ulises.juarez@telasist.com>
2024-11-02 16:10:54 -0600

    config run app

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
 from app import create_app
+import os
+
+
 
 app = create_app()
 
+"""
 if __name__ == '__main__':
     app.run(debug=True)
+"""
+
+
+
+if __name__ == '__main__':
+    # Crea el directorio de UPLOAD_FOLDER si no existe
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    # Ejecuta la aplicación con debug controlado por la variable de entorno
+    app.run(
+        debug=os.environ.get("DEBUG", "False").lower() in ["true", "1"],
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
