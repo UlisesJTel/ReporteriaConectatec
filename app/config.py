70c21038dc8bf35611fbb4a60af6399920c5030b
Ulises Juarez <ulises.juarez@telasist.com>
2024-11-04 09:16:18 -0600

    changing the connection to pymssql

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
-"""from dotenv import load_dotenv
+"""
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -11,17 +12,18 @@ database = os.getenv('DB_NAME')
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        f"mssql+pyodbc://{username}:{password}@{hostname}/{database}?driver=SQL+Server"
+        f"mssql+pymssql://{username}:{password}@{hostname}/{database}"
     )
 
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 """
+
+
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        f"mssql+pyodbc://{os.environ.get('USER')}:{os.environ.get('PASS')}"
-        f"@{os.environ.get('HOST')}/{os.environ.get('DB_NAME')}?driver=SQL+Server"
+        f"mssql+pymssql://{os.environ.get('USERNAME')}:{os.environ.get('PASSWORD')}"
+        f"@{os.environ.get('HOST')}/{os.environ.get('DB_NAME')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
